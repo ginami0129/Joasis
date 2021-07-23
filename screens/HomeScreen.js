8ee09a0f05fa6a6486f0f8309412ca174cc69e61
ginami0129g <ginami0129n@naver.com>
2021-07-23 16:00:07 +0900

    Style : 새글 아이콘 추가

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import Icon from '../components/SquareNIcon';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>공지사항 화면</Text>
+      <Icon name="n_box" size={20} color={'#56286E'} />
     </SafeAreaView>
   );
 };
