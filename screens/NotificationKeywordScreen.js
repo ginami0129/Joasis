20a3ddf2acabdbb41922f0c2935ee437156ea06c
ginami0129g <ginami0129n@naver.com>
2021-08-09 16:35:18 +0900

    Feat : 공지사항 키워드 등록하는 기능 구현

--- screens/NotificationKeywordScreen.js
+++ screens/NotificationKeywordScreen.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  Alert,
+} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const NotificationKeywordScreen = () => {
+  const [keywords, setKeywords] = useState('');
+  const subscribeTopic = () => {
+    console.log(keywords + '구독');
+    return messaging().subscribeToTopic(keywords);
+  };
+
+  useEffect(() => {
+    messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={keywords}
+        onChangeText={text => setKeywords(text)}
+      />
+      <Button title="제출" onPress={subscribeTopic} />
       <Text>공지사항 키워드 설정화면</Text>
     </SafeAreaView>
   );
@@ -14,6 +40,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
